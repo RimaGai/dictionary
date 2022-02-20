e7dd6167758e707ea6011e5312fd7d09e3c998e0
RimaGai <puolus@gmail.com>
2022-02-20 14:56:08 +0100

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic d-none d-sm-block d-md-none">
-      <div className="Text">/{props.phonetic[0].text}/</div>
+      <div className="Text">{props.phonetic[0].text}/</div>
       <div className="Player mt-3">
         <ReactAudioPlayer src={props.phonetic[0].audio} controls={true} />
       </div>
