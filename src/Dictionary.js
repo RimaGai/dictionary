05ba8415f7deb9adaaf68c3a44a9fbe225cb1708
RimaGai <puolus@gmail.com>
2023-05-23 17:29:11 +0200

    Switch to SheCodes Api for photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,18 +15,18 @@ export default function Dictionary(props) {
     setResults(response.data[0]);
   }
 
-  function handlePexelsResponse(response) {
+  function handleResponse(response) {
     setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
-    let pexelsApiKey =
-      "563492ad6f917000010000019174385542934f8f9a26daa4a4f656a1";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+    let sheCodesApiKey = "f3474034becbfaec1efe101oeef3cft3";
+
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${sheCodesApiKey}`;
+
+    axios.get(sheCodesApiUrl).then(handleResponse);
   }
 
   function handleSubmit(event) {
