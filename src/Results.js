c9b9656518004420af6cd9af61632009f6b3de5f
RimaGai <puolus@gmail.com>
2023-06-03 16:30:24 +0200

    Remove react player

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Meaning from "./Meaning";
-import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
@@ -8,7 +7,6 @@ export default function Results(props) {
       <div className="Results">
         <br />
         <h2>{props.results.word}</h2>
-        <Phonetic phonetic={props.results.phonetics} />
         {props.results.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
